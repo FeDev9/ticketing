e0c1f09fcbf9bb27f5ff406ba2e5f6c69e0948d7
federicocecconi <ghiande-boe0a@icloud.com>
2022-05-25 19:21:08 +0200

    check test

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 it('returns a 404 if ticket is not found', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
   const response = await request(app)
-    .get(`api/tickets/${id}`)
+    .get(`/api/tickets/${id}`)
     .send()
     .expect(404);
 });
